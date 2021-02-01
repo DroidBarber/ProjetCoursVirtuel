d0134cbd576fade3620ec05740147954acc3821b
Vivien <viviendu03@live.fr>
2021-02-01 18:43:38 +0100

    Ajout des commentaires de la récupération d'image du diapo en ligne

--- Assets/Scripts/SlideController.cs
+++ Assets/Scripts/SlideController.cs
@@ -9,26 +9,24 @@ public class SlideController : MonoBehaviour
 {
     // Start is called before the first frame update
     public List<Texture2D> diapo = new List<Texture2D>();
-    private Material mat;
+    private Material material;
     private Renderer rendererObj;
     private int id_diapo_active = 0;
 
     private void Awake()
     {
-        if (this.GetComponent<Renderer>())
+        if (this.GetComponent<Renderer>()) // Test si le Component existe sur l'objet courant
         {
-            rendererObj = this.GetComponent<Renderer>(); //récuperation du renderer
-            rendererObj.enabled = false; // par défaut pas d'affichage d'image
-            mat = rendererObj.material; // récuperation du material
-            mat.SetTexture("_MainTex", null); // Pas de texture(=image) par défaut
+            rendererObj = this.GetComponent<Renderer>(); // Récuperation du renderer
+            rendererObj.enabled = false; // Par défaut pas d'affichage d'image
+            material = rendererObj.material; // Récuperation du material
+            material.SetTexture("_MainTex", null); // Pas de texture(=image) par défaut
+            StartCoroutine(GetDiapo());
         }
         else
         {
-            Debug.LogError("Missing Renderer");
+            Debug.LogError("Renderer manquant pour le " + this.name);
         }
-
-        StartCoroutine(Check());
-
     }
 
     void Start()
@@ -42,60 +40,62 @@ public class SlideController : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.P))//appuyer sur M pour activer ou desactiver le diapo
         {
             rendererObj.enabled = !rendererObj.enabled;
-            mat.SetTexture("_MainTex", diapo[id_diapo_active]);
-            Debug.Log(id_diapo_active);
+            if (diapo.Count != 0)
+            {
+                material.SetTexture("_MainTex", diapo[id_diapo_active]);
+            }
+            else
+            {
+                material.SetTexture("_MainTex", null);
+            }
         }
-        else if (Input.GetKeyUp(KeyCode.O)) //diapo 1
+        else if (Input.GetKeyUp(KeyCode.O)) //diapo suivante
         {
             id_diapo_active = (id_diapo_active + 1) % diapo.Count;
-            Debug.Log(id_diapo_active);
-            mat.SetTexture("_MainTex", diapo[id_diapo_active]);
+            material.SetTexture("_MainTex", diapo[id_diapo_active]);
         }
-        else if (Input.GetKeyUp(KeyCode.I)) //diapo 2
+        else if (Input.GetKeyUp(KeyCode.I)) //diapo précédente
         {
             id_diapo_active = (id_diapo_active - 1) >= 0 ? id_diapo_active - 1 : diapo.Count - 1;
-            Debug.Log(id_diapo_active);
-            mat.SetTexture("_MainTex", diapo[id_diapo_active]);
-            mat.SetTexture("_MainTex", diapo[id_diapo_active]);
+            material.SetTexture("_MainTex", diapo[id_diapo_active]);
         }
     }
 
-    private IEnumerator Check()
+    private IEnumerator GetDiapo()
     {
 
         UnityWebRequest www = UnityWebRequest.Get("https://pastebin.com/raw/8e3DsJ2V");
-        yield return www.SendWebRequest();
+        yield return www.SendWebRequest(); // envoie de la requète à l'URL
 
-        if (www.isNetworkError || www.isHttpError)
+        if (www.isNetworkError || www.isHttpError) // Si un problème de connexion
         {
             Debug.Log(www.error);
         }
         else
         {
-            // Show results as text
-            Debug.Log(www.downloadHandler.text);
+            // Affichage dans la console des données reçues
+            //Debug.Log(www.downloadHandler.text);
 
-            // Or retrieve results as binary data
-            byte[] results = www.downloadHandler.data;
-        }
+            // On passe d'un string contenant l'ensemble des URL, à une liste de string contenenat une URL chacun
+            string lst_image = www.downloadHandler.text;
+            List<string> lines = new List<string>(
+                lst_image.Split(new string[] { "\r", "\n" },
+                StringSplitOptions.RemoveEmptyEntries));
 
-        string lst_image = www.downloadHandler.text;
-        List<string> lines = new List<string>(
-            lst_image.Split(new string[] { "\r", "\n" },
-            StringSplitOptions.RemoveEmptyEntries));
-
-        foreach (string url in lines)
-        {
-            www = UnityWebRequestTexture.GetTexture(url);
-            yield return www.SendWebRequest();
-
-            if (www.isNetworkError || www.isHttpError)
+            // Pour chaque URL d'image, on la télécharge et la range dans la variable diapo
+            foreach (string url in lines)
             {
-                Debug.Log(www.error);
-            }
-            else
-            {
-                diapo.Add(((DownloadHandlerTexture)www.downloadHandler).texture);
+                www = UnityWebRequestTexture.GetTexture(url);
+                yield return www.SendWebRequest();
+
+                if (www.isNetworkError || www.isHttpError)
+                {
+                    Debug.Log(www.error);
+                }
+                else
+                {
+                    diapo.Add(((DownloadHandlerTexture)www.downloadHandler).texture);
+                }
             }
         }
     }
