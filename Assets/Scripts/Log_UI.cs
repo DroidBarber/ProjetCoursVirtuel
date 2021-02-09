086fe0fa23dbecd1cdc818c1339e0749a35cd871
Mehdi Barim <60230244+DroidBarber@users.noreply.github.com>
2021-02-09 15:45:32 +0100

    Interface Texte
    Ajout de l'interface texte Debug.log

---
+++ Assets/Scripts/Log_UI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Log_UI : MonoBehaviour
+{
+   private List<string> ListeLog;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Game
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.GameO
+    }
+    public void AjoutLog()
+    {
+        
+    }
+}
