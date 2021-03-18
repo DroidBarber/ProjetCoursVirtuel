36f8b8077d555a1c2587a2ae4253b91b9b554a44
Baptiste <baptiste.foot87@gmail/com>
2021-03-18 11:35:35 +0100

    Pointeur début ok

--- Assets/Scripts/Scripts Scene Menu/MenuChoixScript.cs
+++ Assets/Scripts/Scripts Scene Menu/MenuChoixScript.cs
@@ -5,14 +5,28 @@ using UnityEngine;
 public class MenuChoixScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    // Start is called before the first frame update
+    public GameObject menu;
+    public GameObject EventSystem;
+    public GameObject CameraRig;
+    public GameObject Curseur;
+
     void Start()
     {
-        
+        menu.SetActive(true);// on active ou non le menu (le canvas) 
+        EventSystem.GetComponent<UnityEngine.EventSystems.OVRInputModule>().enabled = true;
+        CameraRig.GetComponent<UnityEngine.EventSystems.OVRPhysicsRaycaster>().enabled = true;
+        Curseur.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void FixedUpdate()
+    {
+
     }
 }
