f2b847dec2794e8756c6bb946d59d3d84cfe4020
Baptiste <baptiste.foot87@gmail/com>
2021-03-30 16:50:13 +0200

    Liste Boutons avec script

---
+++ Assets/Scripts/Scripts Scene Menu/ListeBoutonController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListeBoutonController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject buttonTemplate;
+
+   
+
+     void Start()
+    {
+        for(int i = 1; i<= 20; i++)
+        {
+            GameObject button = Instantiate(buttonTemplate) as GameObject;
+            button.SetActive(true);
+
+            button.GetComponent<ListeBouton>().SetText("Button m" + i);
+
+            button.transform.SetParent(buttonTemplate.transform.parent, false);
+
+
+        }
+        
+    }
+
+    public void ButtonClicked(string myTextString) 
+    { 
+
+    }
+
+    
+}
