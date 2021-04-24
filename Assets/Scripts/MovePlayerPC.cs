65068da974955cb51c36a93edaea48529254c443
Vivien <viviendu03@live.fr>
2021-04-24 16:52:25 +0200

    Pour bouger avec pc

---
+++ Assets/Scripts/MovePlayerPC.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayerPC : MonoBehaviour
+{
+    public float speedMove = 1.0f;
+    public float speedRotate = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        pos *= Time.deltaTime * speedMove;
+        this.transform.Translate(pos, Space.Self);
+
+        Vector3 rot = new Vector3(0, Input.GetAxis("Mouse X"), 0);
+        rot *= Time.deltaTime * speedRotate;
+        this.transform.Rotate(rot, Space.Self);
+    }
+}
